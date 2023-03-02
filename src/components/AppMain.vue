52152477843c3a4c8606f159a725a47f70303f96
MonicaDeBona <debona.monica@outlook.it>
2023-03-02 17:39:03 +0100

    create AppMain and ProjectCard, call axios, add props

---
+++ src/components/AppMain.vue
@@ -0,0 +1,53 @@
+<script>
+import axios from 'axios';
+
+import ProjectCard from './ProjectCard.vue';
+export default {
+    name: 'AppMain',
+    components: {
+        ProjectCard,
+    },
+    data() {
+        return {
+            projects: [],
+            apiUrl: 'http://127.0.0.1:8000/api/projects/',
+        }
+    },
+    methods: {
+        getProjects() {
+            axios.get(this.apiUrl, {
+                params: {
+
+                }
+            })
+                .then((response) => {
+                    this.projects = (response.data.results.data);
+
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        }
+    },
+    created() {
+        this.getProjects();
+    },
+}
+</script>
+
+<template >
+    <section class="container">
+        <div class="row">
+            <div class="col-12">
+                Projects
+            </div>
+        </div>
+
+        <div class="row">
+            <ProjectCard v-for="project in projects" :title="project.title" :image="project.image"
+                :content="project.content" :type="project.type" :author="project.author" />
+        </div>
+    </section>
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        title: String,
+        image: String,
+        content: String,
+        type: String,
+        author: String,
+    },
+}
+</script>
+<template>
+    <div class="card single-project">
+        <div class="card-header text-center">
+            <h3 class="card-title">{{ title }}</h3>
+        </div>
+        <div class="card-body d-flex flex-column justify-content-center align-items-center">
+            <div class="card-image">
+                <img :src="image">
+            </div>
+            <p class="card-text text-center">{{ content }}</p>
+            <ul class="list-unstyled text-center mb-4">
+                <li class="text-muted">Type: {{ type }}</li>
+                <li class="text-muted">Author: {{ author }}</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+</style>
